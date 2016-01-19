b6bac42c43ab6e8cfb110f0195b2c9563b9e570b
Isaiah <icsolomo@ucsc.edu>
2016-01-19 12:13:21 -0800

    changed handler

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
    "fmt"
    "log"
    "net/http"
-   "encoding/json"
-   "os/exec"
 )
 
 type Event struct {
@@ -13,34 +11,23 @@ type Event struct {
    Summary string `json:"summary"`
 }
 
-func getHandlerFunc() func(w http.ResponseWriter, r *http.Request) {
-   handlerFunc := func(w http.ResponseWriter, r *http.Request) {
-      switch r.Method {
-         case "GET":
-            w.Write([]byte("get\n"))
-         case "POST":
-            w.Write([]byte("post\n"))
-         case "PUT":
-            w.Write([]byte("put\n"))
-         case "DELETE":
-            w.Write([]byte("delete\n"))
-         default:
-            w.Write([]byte("default\n"))
-      }
-   }
-   return handlerFunc
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	 case "GET":
+	    w.Write([]byte("get\n"))
+	 case "POST":
+	    w.Write([]byte("post\n"))
+	 case "PUT":
+	    w.Write([]byte("put\n"))
+	 case "DELETE":
+	    w.Write([]byte("delete\n"))
+	 default:
+	    w.Write([]byte("default\n"))
+	}
 }
 
 func main() {
-   var _, _ = json.Marshal(true)
-   http.HandleFunc("/", getHandlerFunc())
+   http.HandleFunc("/", handlerFunc)
    fmt.Println("Listening...")
-   err := exec.Command("cd", "..").Run()
-   out, err2 := exec.Command("ls", ".").Output()
-   if err != nil || err2 != nil {
-      fmt.Println("ERROR")
-      fmt.Printf("%s\n", err)
-   }
-   fmt.Printf("%s\n", out)
    log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- unit_test.go
+++ unit_test.go
@@ -6,5 +6,4 @@ import (
 )
 
 func TestConnect(t *testing.T) {
-   
 }
