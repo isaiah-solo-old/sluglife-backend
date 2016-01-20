9637b95602d2108e2586f279a218b2ba5976f90a
isayyuhh-s <isaiahsolomon16@gmail.com>
2016-01-19 22:19:38 -0800

    get uses query string and post uses body

--- main.go
+++ main.go
@@ -1,33 +1,50 @@
 package main
 
 import (
-   "fmt"
-   "log"
-   "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
 )
 
 type Event struct {
-   Name string `json:"name"`
-   Summary string `json:"summary"`
+	Name string `json:"name"`
+	Description string `json:"description"`
 }
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	// Parses Request Form
+	form_err := r.ParseForm()
+	if form_err != nil {
+		log.Fatal(form_err)
+		return
+	}
+
+	// Finds what type of Request 'r' is
 	switch r.Method {
-	 case "GET":
-	    w.Write([]byte("get\n"))
-	 case "POST":
-	    w.Write([]byte("post\n"))
-	 case "PUT":
-	    w.Write([]byte("put\n"))
-	 case "DELETE":
-	    w.Write([]byte("delete\n"))
-	 default:
-	    w.Write([]byte("default\n"))
+		case "GET":
+			// Accesses Query String
+			name := r.Form.Get("name")
+			w.Write([]byte(name))
+	 	case "POST":
+			var newevent Event
+			decoder := json.NewDecoder(r.Body)
+			decode_err := decoder.Decode(&newevent)
+			if decode_err != nil {
+				log.Fatal(decode_err)
+			}
+			
+	 	case "PUT":
+	 		w.Write([]byte("put\n"))
+		case "DELETE":
+			w.Write([]byte("delete\n"))
+		default:
+	 		w.Write([]byte("default\n"))
 	}
 }
 
 func main() {
-   http.HandleFunc("/", handlerFunc)
-   fmt.Println("Listening...")
-   log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", handlerFunc)
+	fmt.Println("Listening...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
