b68cab7d64cbfba877e34027b04605e896d753b9
kyle-young <readme.py@gmail.com>
2016-01-25 13:41:00 -0800

    sql styling

--- createdb.sql
+++ createdb.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS `slugapp`;
 USE slugapp;
 CREATE TABLE IF NOT EXISTS `event` (
     PRIMARY KEY (`id`),
-    `name` varchar(255),
-    `description` varchar(255)
+    `name` VARCHAR(255),
+    `description` VARCHAR(255)
     `date` TIMESTAMP
 );
