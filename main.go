946715a2a1684bbc386300d1e00477798412d604
isayyuhh-s <isaiah.c.solomon@gmail.com>
2016-04-17 01:12:19 -0700

    added listening msg

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+  "fmt"
   "net/http"
   "./server"
   "./server/event"
 )
 
 func main() {
+  fmt.Printf("Listening...\n")
   http.ListenAndServe(":8080", server.New(event.NewSimpleStore()))
 }
