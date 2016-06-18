9567ad46185d30cfe4340df60634fe148adac496
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2016-06-18 02:20:45 -0700

    deleted some more files

--- createdb.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `slugapp`;
-USE slugapp;
-CREATE TABLE IF NOT EXISTS `event` (
-    PRIMARY KEY (`id`),
-    `name` VARCHAR(255),
-    `description` VARCHAR(255)
-    `date` TIMESTAMP
-);
--- unit_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-   "testing"
-//   "io/exec"
-)
-
-func TestConnect(t *testing.T) {
-}
